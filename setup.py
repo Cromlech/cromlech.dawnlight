3b50c5443c98289129287b3030bea23f820ebd78
Souheil CHELFOUH <trollfot@gmail.com>
2011-05-16 13:07:11 +0200

    Made the lookups pluggable

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup, find_packages
-import os
+# -*- coding: utf-8 -*-
 
-version = '0.1a1'
+import os
+from setuptools import setup, find_packages
+
+version = '0.1a2'
 
 install_requires = [
     'grokcore.component >= 2.3',
@@ -13,30 +15,32 @@ install_requires = [
 
 tests_require = [
     'pytest',
+    'cromlech.browser [test]',
     ]
 
-setup(name='cromlech.dawnlight',
-      version=version,
-      description="Dawnlight publisher for Cromlech Web Framework",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
+setup(
+    name='cromlech.dawnlight',
+    version=version,
+    description="",
+    long_description=open("README.txt").read() + "\n" +
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
         "Programming Language :: Python",
         ],
-      keywords='Cromlech Dolmen Framework',
-      author='The Dolmen team',
-      author_email='dolmen@list.dolmen-project.org',
-      url='http://gitweb.dolmen-project.org/',
-      license='ZPL',
-      packages=find_packages('src', exclude=['ez_setup']),
-      package_dir={'': 'src'},
-      namespace_packages=['cromlech'],
-      include_package_data=True,
-      zip_safe=False,
-      tests_require=tests_require,
-      install_requires=install_requires,
-      extras_require={'test': tests_require},
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    keywords='Cromlech Publisher',
+    author='The Dolmen team',
+    author_email='dolmen@list.dolmen-project.org',
+    url='http://gitweb.dolmen-project.org/',
+    license='ZPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    namespace_packages=['cromlech'],
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=tests_require,
+    install_requires=install_requires,
+    extras_require={'test': tests_require},
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )
