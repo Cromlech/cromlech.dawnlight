08db64b50a516feadbb36a20cc577c703540320c
Souheil CHELFOUH <trollfot@gmail.com>
2012-01-11 19:55:02 +0100

    Added utils methods

---
+++ src/cromlech/dawnlight/utils.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from cromlech.browser.interfaces import IHTTPRenderer
+from zope.component import queryMultiAdapter
+from zope.location import ILocation, LocationProxy, locate
+
+try:
+    from zope.security.proxy import ProxyFactory
+except ImportError:
+    def ProxyFactory(obj):
+        """A replacement that raises an error.
+        """
+        raise NotImplementedError(
+            'No security factory as been registered. '
+            'Unable to protect %r. Please see `zope.security` '
+            'for more information regarding security proxies.')
+
+
+def query_http_renderer(request, obj, name=""):
+    return queryMultiAdapter((obj, request), IHTTPRenderer, name=name)
+
+
+def renderer_locator(func):
+    """Can be used as a decorator on the `__call__` method of the
+    `ViewLookup` component. It provides a way to relate the looked
+    up renderer with its lookup context.
+    """
+    def locate_renderer(request, obj, stack):
+        renderer = func(request, obj, stack)
+        if renderer is not None:
+            if not ILocation.providedBy(renderer):
+                renderer = LocationProxy(renderer)
+            locate(renderer, name=name, parent=obj)
+        return renderer
+    return locate_renderer
+
+
+def renderer_protector(func):
+    """Can be used as a decorator on the `__call__` method of the
+    `ViewLookup` component. It provides a way to wrap the looked
+    up renderer in a security proxy, security the component accesses.
+    """
+    def protect_renderer(request, obj, stack):
+        renderer = func(request, obj, stack)
+        if renderer is not None:
+            return ProxyFactory(renderer)
+        return renderer
+    return protect_renderer
